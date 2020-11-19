1547deefc0af3b1c334135d8a5f7f95f56655e34
jessicadever1 <jessicadever1@gmail.com>
2020-11-19 12:01:57 -0600

    partially completed new arrays of Holy, Solider and Unworthy Fish

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -3,7 +3,7 @@ const fishCollection = [
         image: "Bruce.png",
         name: "Bruce",
         species: "Great White Shark",
-        length: "6 feet",
+        length: "72",
         location: "Great Barrier Reef",
         diet: "Not fish"
     },
@@ -12,7 +12,7 @@ const fishCollection = [
         image: "Flounder.png",
         name: "Flounder",
         species: "Blue Angelfish",
-        length: "6 inches",
+        length: "6",
         location: "Disney",
         diet: "Algea"
     },
@@ -21,7 +21,7 @@ const fishCollection = [
         image: "Ariel.png",
         name: "Ariel",
         species: "Mermaid",
-        length: "5 feet, 1 inch",
+        length: "61",
         location: "Disney",
         diet: "Cake"
     },
@@ -30,7 +30,7 @@ const fishCollection = [
         image: "Nemo.jpeg",
         name: "Nemo",
         species: "Clownfish",
-        length: "3 inches",
+        length: "3",
         location: "Unknown - He is lost",
         diet: "Algea"
     },
@@ -39,7 +39,7 @@ const fishCollection = [
         image: "Dory.jpeg",
         name: "Dory",
         species: "Royal Blue Tang",
-        length: "5 inches",
+        length: "5",
         location: "The Great Barrier Reef",
         diet: "Algea"
     },
@@ -48,12 +48,44 @@ const fishCollection = [
         image: "Marlin.jpeg",
         name: "Marlin",
         species: "Clownfish",
-        length: "4 inches",
+        length: "4",
         location: "The Great Barrier Reef",
         diet: "Algea"
     }
 ]
 
-export const useFish = () => {
-    return fishCollection.slice()
-}
+// export const useFish = () => {
+//     return fishCollection.slice()
+// }
+
+// const holyFish = []
+// const soldierFish = []
+// const unworthyFish = []
+
+// for (const fish of fishCollection) {
+//     if (`${fish.length}` % 3 === 0) {
+//         holyFish.push(fish.name)
+//     }   else if (`${fish.length}` % 5 === 0) {
+//         soldierFish.push(fish.name)
+//     }   else {
+//         unworthyFish.push(fish.name)
+//     }
+// }
+
+// console.log(`The holy fish are ${holyFish}, 
+// the solider fish are ${soldierFish},
+//  and the unworthy fish are ${unworthyFish}`)
+
+    const mostHolyFish = () => {
+        const holyFish = []
+
+        for (const fish of fishCollection) {
+            if (`${fish.length}` % 3 === 0) {
+                holyFish.push(fish.name) 
+            }
+        }
+        return holyFish
+    }
+
+const returnValueOfMostHoly = mostHolyFish()
+console.log(returnValueOfMostHoly);
